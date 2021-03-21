088ab7db50f44e85ff533a7bbaeeb1459fb72efa
NicolayRomanovichRobo <79041818+KokovkinNR@users.noreply.github.com>
2021-03-21 16:50:18 +0300

    lesson3

---
+++ lib/WalkerLegs/WalkerLegs.cpp
@@ -0,0 +1,15 @@
+#include "WalkerLegs.h"
+
+WalkerLegs::WalkerLegs()
+{
+    servo = 4;
+    servoCenterConst[0] = 90;
+    servoCenterConst[1] = 90;
+    servoCenterConst[2] = 90;
+    servoCenterConst[3] = 90;
+}
+
+WalkerLegs::WalkerLegs(int a0, int a1, int a2, int a3)
+{
+    
+}
